63e98858e92ed572b98ef03b14c0cbaf5158bb34
vietng <vietng322611@gmail.com>
2026-09-19 13:35:04 +0700

    misc: register customer and employee services

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,3 +2,5 @@ export * from './auth.service';
 export * from './booking.service';
 export * from './court.service';
 export * from './dashboard.service';
+export * from './customer.service';
+export * from './employee.service';
